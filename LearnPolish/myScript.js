883733a79a6c6945540de2f799492cc8472f57a3
Twinkerxd <msv-oo@yandex.ru>
2022-09-01 15:44:57 +0200

    added new words

--- LearnPolish/myScript.js
+++ LearnPolish/myScript.js
@@ -1,9 +1,16 @@
 const ruWords = ["бояться","играть","брать","просыпаться","быть","хотеть","ходить","ждать","чувствовать","чувствовать себя",
-"чистить","читать","узнать","готовить, варить","идти","ехать","есть","врать","любить","резать","покупать","лететь","считать","любить","волноваться","мечтать","иметь"];
+"чистить","читать","узнать","готовить, варить","идти","ехать","есть","врать","любить(кого-то)","резать","покупать","лететь","считать","любить(что-то)",
+"волноваться","мечтать","иметь",
+"молчать","мочь","говорить","мыть","думать","ремонтировать","отдавать","отдыхать","ответить","смотреть (что-то)","рассказывать","открывать",
+"открыть","курить","пить","писать","платить","плавать","пойти","помогать","суметь","позволить","работать","пробовать","производить"];
 const plWords = ["bać się","bawić się","brać","budzić się","być","chcieć","chodzić","czekać","czuć","czuć się",
-"czyścić","czytać","dowiedzieć się","gotować","iść","jechać","jeść","kłamać","kochać","kroić","kupować","lecieć","liczyć","lubić","martwić się","marzyć","mieć"];
+"czyścić","czytać","dowiedzieć się","gotować","iść","jechać","jeść","kłamać","kochać","kroić","kupować","lecieć","liczyć","lubić"
+,"martwić się","marzyć","mieć",
+"milczeć","móc","mówić","myć","myśleć","naprawiać","oddawać","odpoczywać","odpowiedzieć","oglądać","opowiadać","otwierać",
+"otworzyć","palić","pić","pisać","płacić","pływać","pójść","pomagać","potrafić","pozwolić","pracować","próbować","produkować"];
 
 var random = null;
+console.log("Word counts: " + plWords.length);
 
 function clickOK() {
     var count = parseInt(document.getElementById("count").value, 10);
