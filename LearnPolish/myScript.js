99e9028ee63b48b677e6bf2f38ee047ad6eba99f
Twinkerxd <msv-oo@yandex.ru>
2022-09-01 10:14:28 +0200

    some refactor

--- LearnPolish/myScript.js
+++ LearnPolish/myScript.js
@@ -1,28 +1,32 @@
+const ruWords = ["бояться","играть","брать","просыпаться","быть","хотеть","ходить","ждать","чувствовать","чувствовать себя",
+"чистить","читать","узнать","готовить, варить","идти","ехать","есть","врать","любить","резать","покупать","лететь","считать","любить","волноваться","мечтать","иметь"];
+const plWords = ["bać się","bawić się","brać","budzić się","być","chcieć","chodzić","czekać","czuć","czuć się",
+"czyścić","czytać","dowiedzieć się","gotować","iść","jechać","jeść","kłamać","kochać","kroić","kupować","lecieć","liczyć","lubić","martwić się","marzyć","mieć"];
+
+var random = null;
+
 function clickOK() {
+    var count = parseInt(document.getElementById("count").value, 10);
     var s = document.getElementById("1").value;
 
-    if (s == "jeść") {
-        console.log("правильно");
+    if (s == plWords[random]) {
+        document.getElementById("2").value = "правильно";
+        document.getElementById("count").value = count + 1;
+        changeWord();
     } else {
-        console.log("не правильно");
+        document.getElementById("2").value = "не правильно";
+        document.getElementById("3").value = plWords[random];
     }
-
-    // bieganie
-    // czekać
-    // jeść
 }
 
-function read() {
-    contacts='[{"name":"bla bla", "email":bla bla, "address":"bla bla"}]';
-    var mydata = JSON.parse(contacts);
-    alert(mydata[0].name);
+function changeWord() {
+      document.getElementById("1").value = "";
+      document.getElementById("2").value = "";
+      document.getElementById("3").value = "";
+      document.getElementById("0").value = ruWords[myRandom()];
 }
 
-function changeImage() {
-    document.getElementById("img").src = "files/"+random()+".png";
-    read();
-}
-
-function random() {
-    return Math.floor(Math.random() * 3 + 1);
+function myRandom() {
+    random = Math.floor(Math.random() * (plWords.length - 1));
+    return random;
 }
